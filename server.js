014f32fba70af0f16b025d2c1f4baa6cf231d04b
cgriffinhub <chuckgriffin1987@gmail.com>
2019-03-02 17:37:00 -0500

    upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var db = require("./models");
 var bodyParser = require("body-parser");
-var PORT = process.env.PORT || 8000;
+var port = process.env.PORT || 8000;
 var app = express();
 
 var db = require("./models");
@@ -26,7 +26,8 @@ app.use(routes);
 
 //Syncing our sequelize models and then starting our express app
 db.sequelize.sync().then(function(){
-	app.listen(PORT, function(){
-		console.log("Listening on port %s", PORT);
+	app.listen(port, function() {
+		console.log("App is running on port " + port);
 	});
+	
 });
